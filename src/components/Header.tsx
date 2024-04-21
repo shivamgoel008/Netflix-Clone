b6e42d388b8a32436dae9db2b460e74f4ffb5244
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-04-22 00:55:47 +0530

    constant addded

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import { auth } from "../utils/firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { DEFAULT_AVATAR, LOGO } from "../utils/constants";
 
 const Header = () => {
   const user = useSelector((appStore: any) => appStore.user);
@@ -42,7 +43,7 @@ const Header = () => {
       <div className="w-full absolute px-48 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
         <img
           className="w-44"
-          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          src={LOGO}
           alt="logo"
         />
 
@@ -51,7 +52,7 @@ const Header = () => {
             className="w-12 h-12 m-2 rounded-lg"
             src={
               user?.photoURL == null
-                ? "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp"
+                ? DEFAULT_AVATAR
                 : user.photoURL
             }
             alt="user logo"
@@ -67,7 +68,3 @@ const Header = () => {
 };
 
 export default Header;
-function dispatch(arg0: any) {
-  throw new Error("Function not implemented.");
-}
-
