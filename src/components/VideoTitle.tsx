1ef1612d8ffa308ea65589ab925dc97e04802aa0
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-04-30 01:24:17 +0530

    early return, title and overivew as child complete

---
+++ src/components/VideoTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoTitle = ({title, overview}:any) => {
+    console.log(title);
+    console.log(overview)
+  return (
+    <div>VideoTitle</div>
+  )
+}
+
+export default VideoTitle
