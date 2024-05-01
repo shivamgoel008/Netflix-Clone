ce4ac6d51415802965f397e719268760a8b558b4
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-05-02 00:28:28 +0530

    css added

--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -4,7 +4,18 @@ const VideoTitle = ({title, overview}:any) => {
     console.log(title);
     console.log(overview)
   return (
-    <div>VideoTitle</div>
+    <div className='pt-36 px-36'>
+      <h1 className='text-6xl font-bold'>{title}</h1>
+      <p className='py-6 text-lg w-1/4'>{overview}</p>
+      <div>
+        <button className='bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-xl'>
+         ▶️ Play
+        </button>
+        <button className='mx-2  bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-xl'>
+          More Info
+        </button>
+      </div>
+    </div>
   )
 }
 
