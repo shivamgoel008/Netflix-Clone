58bc156638432871a09555c2f0a009d4aac4560f
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-05-03 00:57:50 +0530

    mess up cleaned

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -2,29 +2,12 @@ import React, { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }: any) => {
-  const dispatch = useDispatch();
+  
   const trailerVideo = useSelector((store:any)=>store.movies?.trailerVideo);
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-    const filterData = json.results.filter(
-      (videos: any) => videos.type === "Trailer"
-    );
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer))
-  };
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  useMovieTrailer(movieId);
   return (
     <div>
       <iframe
