324910d155a557cc746127bcc478b3df8e5b711f
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-05-05 01:36:12 +0530

    css fixed

--- src/components/SecondaryConatiner.tsx
+++ src/components/SecondaryConatiner.tsx
@@ -5,8 +5,13 @@ import { useSelector } from 'react-redux'
 const SecondaryConatiner = () => {
   const movies = useSelector((store:any)=>store.movies);
   return (
-    <div>
+    <div className=' bg-gray-950'>
+      <div className=' -mt-40 relative z-20'>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+      </div>
     </div>
   )
 }
