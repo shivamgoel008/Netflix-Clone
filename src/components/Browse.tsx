ee583c4317a1dcea52deddb5c55002655ad60712
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-05-05 02:07:05 +0530

    different categories added

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -2,9 +2,14 @@ import Header from './Header'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import MainContainer from './MainContainer';
 import SecondaryConatiner from './SecondaryConatiner';
+import usePopularMovies from '../hooks/useNowPopularMovies';
+import useTopRatedMovies from '../hooks/useUpcomingMovies copy';
+import useUpcomingMovies from '../hooks/useUpcomingMovies';
 const Browse = () => {
   useNowPlayingMovies();
-  
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
   return (
     <div>
       <Header/>
--- src/components/SecondaryConatiner.tsx
+++ src/components/SecondaryConatiner.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import MovieList from './MovieList'
 import { useSelector } from 'react-redux'
 
@@ -8,9 +7,9 @@ const SecondaryConatiner = () => {
     <div className=' bg-gray-950'>
       <div className=' -mt-40 relative z-20'>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/>
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+      <MovieList title={"Top Rated"} movies={movies.topRatedMovies}/>
+      <MovieList title={"Popular"} movies={movies.popularMovies}/>
+      <MovieList title={"Upcoming"} movies={movies.upcomingMovies}/>
       </div>
     </div>
   )
