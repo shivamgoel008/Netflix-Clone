07a3c5e8f7018587e28ec9b06e3879254e266049
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-05-09 01:04:33 +0530

    mess

--- src/components/GptSearchBar.tsx
+++ src/components/GptSearchBar.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import openai from '../utils/openai';
+import React, { useRef } from 'react'
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const   GptSearchBar = () => {
+  const searchText = useRef<HTMLInputElement>(null);
+
+  const handleGptSearchClick = async ()=> {
+    console.log(searchText?.current?.value )
+    const result = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: searchText?.current?.value as string }],
+      model: 'gpt-3.5-turbo',
+    });
+    console.log(result.choices);
+  }
+
   return (
     <div className='pt-[10%] flex justify-center'>
-        <form className='w-1/2 bg-black grid grid-cols-12'>
+        <form className='w-1/2 bg-black grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
             <input
+               ref={searchText}
                type='text'
                className='p-4 m-4 col-span-9 rounded-xl'
                placeholder='What would you like to watch today?'
             />
-            <button className='col-span-3 m-4 py-2 px-4 bg-red-500 text-white rounded-xl'>
+            <button className='col-span-3 m-4 py-2 px-4 bg-red-500 text-white rounded-xl' onClick={handleGptSearchClick}>
                 Search
             </button>
 
