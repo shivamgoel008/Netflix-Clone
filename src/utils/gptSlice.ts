8c0e7ba8e0ec2a22483973f9c7ebd743dbd1574c
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-05-14 01:55:54 +0530

    ai api issue fix, recommendations added

--- src/utils/gptSlice.ts
+++ src/utils/gptSlice.ts
@@ -5,14 +5,18 @@ const gptSlice=createSlice({
     name:'gpt',
     initialState:{
         showGptSearch:false,
+        gptMovies:null,
     },
     reducers:{
         toggleGptSearchView:(state:any)=>{
             state.showGptSearch=!state.showGptSearch;
+        },
+        addGptMovieResults:(state:any, action:any)=>{
+            state.gptMovies=action.payload;
         }
     }
 });
 
-export const {toggleGptSearchView}=gptSlice.actions;
+export const {toggleGptSearchView,addGptMovieResults}=gptSlice.actions;
 
 export default gptSlice.reducer;
