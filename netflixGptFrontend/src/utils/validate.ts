4a68e4b990f138a0b224e1f153b5c375a417f75c
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2024-08-27 00:31:57 +0530

    mess cleanup

--- netflixGptFrontend/src/utils/validate.ts
+++
@@ -1,9 +0,0 @@
-export const checkValidate = (email: string, password: string, name: string, isSignInForm: boolean) => {
-    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
-    const isPasswordValid = /^.{8,}$/.test(password);
-    const isNameValid = name?.length > 3;
-    if (!isEmailValid) return "Email ID is not valid";
-    if (!isPasswordValid) return "Password Not valid";
-    if (!isNameValid && !isSignInForm) return "Name Not Valid";
-    return "";
-}
